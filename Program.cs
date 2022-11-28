fdf559ce52752351f439d8729aeea67080c72533
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2022-11-28 10:19:16 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_16_Prog_120_F22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        } // Main
+
+    } // class
+
+} // namespace
---
+++ README.md
@@ -0,0 +1,2 @@
+# Lecture_16_Prog_120_F22
+ Programming 120 - Fall 22 - Lecture 16
